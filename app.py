6330f123abda1506c32bd2998ce924f9bcaa2c05
sri raj <sriraj14@gmail.com>
2022-10-08 15:35:24 -0700

    changed the page BODY

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World!.. Docker and K8s rocks!'
 
 if __name__ == "__main__":
    app.run()
